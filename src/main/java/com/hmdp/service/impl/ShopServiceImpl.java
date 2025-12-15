b34574e530f370956f2547f4525f0faaed61263d
qiyu-lu <1186387362@qq.com>
2025-12-15 20:35:59 +0800

    商户类型缓存string类型

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,25 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
  * <p>
@@ -16,5 +31,25 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    @Override
+    public Result queryShopById(Long id){
+        //构造查询的id，在redis中的key
+        String shopKey = CACHE_SHOP_KEY + id;
+        //先设置在redis中存的是json格式
+        String shopJson = stringRedisTemplate.opsForValue().get(shopKey);
+        if(StrUtil.isNotBlank(shopJson)){
+            log.debug("从redis中进行查询商铺");
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        Shop shop = getById(id);
+        if(shop == null){
+            return Result.fail("查询商铺不存在");
+        }
+        stringRedisTemplate.opsForValue().set(shopKey, JSONUtil.toJsonStr(shop));
+        return Result.ok(shop);
+    }
 }
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -26,7 +26,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import static com.hmdp.utils.RedisConstants.*;
 import static com.hmdp.utils.RegexUtils.isPhoneInvalid;
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -56,8 +58,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         //存入session  将验证码存入传入的session中
         //session.setAttribute("code", code);
         //这里不是选择将验证码存入session中，而是选择存入redis中
-        String codeKey = "login:code:" + phone;
-        stringRedisTemplate.opsForValue().set(codeKey, code, 2, TimeUnit.MINUTES);
+        String codeKey = LOGIN_CODE_KEY + phone;
+        stringRedisTemplate.opsForValue().set(codeKey, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
 
         //打印或者发送短信
         log.debug("验证码是：" + code);
@@ -73,19 +75,12 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         // 取出验证码 用户输入的验证吗
         String rawCode = loginForm.getCode();
         //和之前发送的验证码进行比较
-        //session 中是之前 后端发送给用户并存session中的验证码，而loginFromDTO中的code是用户手动输入的验证码
-//        if(rawCode ==null || !rawCode.equals(session.getAttribute("code"))){
-//            return Result.fail("验证码不正确");
-//        }
+
         //从redis中取出验证码
-        if(rawCode ==null || !rawCode.equals(stringRedisTemplate.opsForValue().get("login:code:" + phone))){
+        if(rawCode ==null || !rawCode.equals(stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone))){
             return Result.fail("验证码不正确");
         }
         //根据号码查询用户，如果存在返回用户，不存在新建用户
-        // 这里之所以不需要写sql语句，是因为这个类 继承了 MyBatis-Plus 的 ServiceImpl<M extends BaseMapper<T>, T>
-        // UserMapper 继承 BaseMapper，注入的UserMapper 通过 baseMapper 来执行 SQL，
-        // 而为什么User 类可以映射到数据库表，因为在User 类上加了注解 @TableName("tb_user")，
-        // 这个注解告诉 MyBatis-Plus，User 这个实体类对应数据库的 tb_user 表，MP 自动根据字段名生成 SQL
         User user = lambdaQuery()
                 .eq(User::getPhone, phone)
                 .one();
@@ -94,10 +89,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             save(user);
         }
         UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);//复制不隐私的信息，不过我认为这里应该使用vo
-//        session.setAttribute("user", userDTO);//将用户部分信息存入session
-        //这里选择采用将用户保存到redis中，应该以哈希类型进行存储，Hash 可部分更新字段，不需要整个 JSON 反序列化 / 序列化，性能更好
-        //例如刷新 token 续期，只需要更新 expire_time 字段。
-        // 随机生成 token
+
         String token = UUID.randomUUID().toString(true);
         //Bean → Map（去掉敏感字段）Redis Hash 不支持存复杂对象，需要转成 String
         //把 UserDTO 对象转换成一个 Map<String, String>（Redis Hash 需要 String）
@@ -109,7 +101,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
                         .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString())
         );
         //写入 Redis（Hash 类型）+ 设置 TTL
-        String tokenKey = "login:token:" + token;
+        String tokenKey = LOGIN_USER_KEY + token;
         stringRedisTemplate.opsForHash().putAll(tokenKey, userMap);
         stringRedisTemplate.expire(tokenKey, 30, TimeUnit.MINUTES);// 设置有效期（30 分钟）
 
@@ -120,7 +112,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     private User generateUserWithphone(String phone){
         User user = new User();
         user.setPhone(phone);
-        user.setNickName("user_" + RandomUtil.randomString(10));
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
         user.setCreateTime(LocalDateTime.now());
         user.setUpdateTime(LocalDateTime.now());
         return user;
